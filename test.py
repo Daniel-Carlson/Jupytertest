4bb429e4fd7345b21785e81ea5c1a19a122162d5
Daniel Carlson <51524233+Daniel-Carlson@users.noreply.github.com>
2021-04-15 01:25:46 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-x=10
+import pandas as pd
+
+cars = {'Brand': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
+        'Price': [22000,25000,27000,35000]
+        }
+
+df = pd.DataFrame(cars, columns = ['Brand', 'Price'])
